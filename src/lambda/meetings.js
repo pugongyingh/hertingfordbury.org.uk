c4d20d7890bf65f753ffc5435f1f913efd45edcc
Richard Herbert <richard@infoweb.co.uk>
2019-01-10 13:37:26 +0000

    Move content-type to callback()

--- src/lambda/meetings.js
+++ src/lambda/meetings.js
@@ -2,11 +2,12 @@ const axios = require( 'axios' );
 const API_ENDPOINT = 'https://api.subsume.io/hertingfordbury/v1/meetings';
 
 exports.handler = ( event, context, callback ) => {
-	axios.get( API_ENDPOINT, {
-			responseType: 'json'
-		} )
+	axios.get( API_ENDPOINT )
 		.then( ( response ) => {
 			callback( null, {
+				headers: {
+					content-type: 'application/json;charset=utf-8'
+				},
 				statusCode: 200,
 				body: JSON.stringify( response.data )
 			} );
